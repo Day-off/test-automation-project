32b83b815afb11477355993dfc0f8860b42423d9
krsobo <krsobo@ttu.ee>
2022-12-04 17:52:02 +0200

    Cleaning Code

--- src/main/java/org/app/FullWeatherReport.java
+++ src/main/java/org/app/FullWeatherReport.java
@@ -1,13 +1,6 @@
 package org.app;
 
 import lombok.extern.java.Log;
-import org.app.api.WeatherApi;
-import org.app.api.dto.CurrentWeatherReportDto;
-import org.app.api.dto.ForecastReportDto;
-import org.app.api.dto.MainDetailsDto;
-import org.app.api.dto.details.WeatherReport;
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 import java.io.*;
@@ -19,25 +12,9 @@ import java.util.Scanner;
 public class FullWeatherReport {
 
     public JSONObject getFullWeatherReport(String city) {
-        try {
-            MainDetailsDto mainDetailsDto = new WeatherApi().getMainDataDto(city);
-            CurrentWeatherReportDto currentWeatherReportDto = new WeatherApi().getCurrentWeatherReportDto(city);
-            ForecastReportDto freeDaysForecastDto = new WeatherApi().getFreeDaysForecastDto(city);
-
-            JSONObject output = new JSONObject();
-
-            setMainDetailsToOutPut(mainDetailsDto, output);
-            setCurrentWeatherReportToOutPut(currentWeatherReportDto, output);
-            setForecastReportToOutPut(freeDaysForecastDto, output);
-
-
-            log.info(output.toString());
-
-            return output;
-
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
+        Report report = new Report(city);
+        log.info(report.getOutput().toString());
+        return report.getOutput();
     }
 
     public JSONObject getReportFromStdin() {
@@ -100,60 +77,6 @@ public class FullWeatherReport {
 
     private void checkNotNull(String fullName) throws Exception {
         isValidFile(fullName.isEmpty(), "FILE NAME IS EMPTY!\n");
-    }
-
-    private static JSONArray setForecastReports(ForecastReportDto freeDaysForecastDto) throws JSONException {
-        JSONArray forecastReport = new JSONArray();
-        for (WeatherReport weatherReport : freeDaysForecastDto.getFreeDaysReports()) {
-            JSONObject report = new JSONObject();
-            JSONObject weather = new JSONObject();
-            setForecastWeatherReport(forecastReport, weatherReport, report, weather);
-        }
-
-        return forecastReport;
-    }
-
-    private static void setForecastReportToOutPut(ForecastReportDto freeDaysForecastDto, JSONObject output) throws JSONException {
-        JSONArray forecastReport = setForecastReports(freeDaysForecastDto);
-        output.put("forecastReport", forecastReport);
-    }
-
-    private static void setForecastWeatherReport(JSONArray forecastReport, WeatherReport weatherReport, JSONObject report, JSONObject weather) throws JSONException {
-        report.put("date", weatherReport.getDt());
-        weather.put("temperature", weatherReport.getMain().getTemp());
-        weather.put("humidity", weatherReport.getMain().getHumidity());
-        weather.put("pressure", weatherReport.getMain().getPressure());
-        report.put("weather", weather);
-        forecastReport.put(report);
-    }
-
-    public static void setCurrentWeatherReportToOutPut(CurrentWeatherReportDto currentWeatherReportDto, JSONObject output) throws JSONException {
-        JSONObject currentWeatherReport = new JSONObject();
-        setCurrentWeatherReport(currentWeatherReportDto, currentWeatherReport);
-        output.put("currentWeatherReport", currentWeatherReport);
-    }
-
-    private static void setCurrentWeatherReport(CurrentWeatherReportDto currentWeatherReportDto, JSONObject currentWeatherReport) throws JSONException {
-        currentWeatherReport.put("date", currentWeatherReportDto.getDate());
-        currentWeatherReport.put("temperature", currentWeatherReportDto.getMain().getTemp());
-        currentWeatherReport.put("humidity", currentWeatherReportDto.getMain().getHumidity());
-        currentWeatherReport.put("pressure", currentWeatherReportDto.getMain().getPressure());
-    }
-
-    public void setMainDetailsToOutPut(MainDetailsDto mainDetailsDto, JSONObject output) throws JSONException {
-        try {
-            JSONObject mainDetails = new JSONObject();
-            setMainWeatherDetails(mainDetailsDto, mainDetails);
-            output.put("mainDetails", mainDetails);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void setMainWeatherDetails(MainDetailsDto mainDetailsDto, JSONObject mainDetails) throws JSONException {
-        mainDetails.put("city", mainDetailsDto.getCity());
-        mainDetails.put("coordinates", mainDetailsDto.getCoordinates().getLatAndLon());
-        mainDetails.put("temperatureUnit", mainDetailsDto.getMain().getTemperatureUnit());
     }
 
 }
